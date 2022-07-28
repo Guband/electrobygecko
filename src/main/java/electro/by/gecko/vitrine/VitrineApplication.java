bdb601b4651c693fce29732b00b97b157a78fcfe
darkikim <maxime.goethals.59@gmail.com>
2022-07-28 11:16:09 +0200

    Initial set of files

---
+++ src/main/java/electro/by/gecko/vitrine/VitrineApplication.java
@@ -0,0 +1,13 @@
+package electro.by.gecko.vitrine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitrineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitrineApplication.class, args);
+	}
+
+}
