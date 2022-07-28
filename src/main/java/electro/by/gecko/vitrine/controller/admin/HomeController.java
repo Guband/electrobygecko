ee5c4626d1a0a4d0a7df92977b568b9e5955e364
zephyrgate <98980080+zephyrgate@users.noreply.github.com>
2022-07-28 16:41:02 +0200

    Added Bootstrap as Maven dependency + Initialized Admin ProductController (list)

---
+++ src/main/java/electro/by/gecko/vitrine/controller/admin/HomeController.java
@@ -0,0 +1,15 @@
+package electro.by.gecko.vitrine.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/admin")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "redirect:/admin/products/list";
+    }
+}
