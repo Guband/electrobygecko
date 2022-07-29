14c762afce747f2dc9c36e3cff728bc16d41fe65
darkikim <maxime.goethals.59@gmail.com>
2022-07-29 16:13:46 +0200

    Configuration for webjars && upload-dir folder to be available in web

---
+++ src/main/java/electro/by/gecko/vitrine/config/UploadConfig.java
@@ -0,0 +1,34 @@
+package electro.by.gecko.vitrine.config;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class UploadConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/")
+                .resourceChain(false);
+        registry.setOrder(1);
+        exposeDirectory("upload-dir", registry);
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file://"+ uploadPath + "/");
+    }
+}
+
+
